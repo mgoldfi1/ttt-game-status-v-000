2c82bd28e214e9a132e7a290b11322a3eb0df538
Matthew Goldfine <mgoldfi1@binghamton.edu>
2018-06-14 18:15:58 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -23,6 +23,9 @@ def won?(board)
   position_1 = board[win_index_1]
   position_2 = board[win_index_2]
   position_3 = board[win_index_3]
+  if position_1 == "X" && position_2 == "X"  && position_3 == "X"
+    top_row
+  end
   if WIN_COMBINATIONS.none?
     false
   end
